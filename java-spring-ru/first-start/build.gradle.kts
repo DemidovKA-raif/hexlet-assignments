5e0b1e604cca46bbc88c48247bff809d1738b55c
@hexlet/cli <support@hexlet.io>
2024-03-05 09:39:44 +0300

    submit java-spring-ru/first-start

--- java-spring-ru/first-start/build.gradle.kts
+++ java-spring-ru/first-start/build.gradle.kts
@@ -3,13 +3,13 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     // BEGIN
-    
-    // END
-    application
-    id("com.github.ben-manes.versions") version "0.48.0"
     id("java")
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.3"
+    // END
+    application
+    id("com.github.ben-manes.versions") version "0.48.0"
+
 }
 
 group = "exercise"
@@ -24,15 +24,14 @@ repositories {
 
 dependencies {
     // BEGIN
-    
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
     // END
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.hamcrest:hamcrest:2.2")
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.test {

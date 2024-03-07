e9130bc88a87fbf3c327436e6a6a0c498c650d9d
@hexlet/cli <support@hexlet.io>
2024-03-07 20:31:29 +0300

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,6 +1,7 @@
 package exercise.controller.users;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +16,24 @@ import exercise.Data;
 @RequestMapping("/api")
 public class PostsController {
 
+    List<Post> posts = Data.getPosts();
+
     @GetMapping("/users/{id}/posts")
-    public List<Post> posts(@PathVariable int id) {
-        var posts = Data.getPosts();
+        public ResponseEntity<Object> posts(@PathVariable int id) {
         List<Post> userPosts = posts.stream()
-                .filter(p -> p.getUserId() == id).toList();
-            return userPosts;
-        }
-
-
+                .filter(p -> p.getUserId() == id)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(userPosts);
+    }
 
     @PostMapping("/users/{id}/posts") // Создание страницы
-    public ResponseEntity<Object> create(@PathVariable int id, @RequestBody Post data) {
-        List<Post> posts = Data.getPosts();
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Post> create(@PathVariable int id, @RequestBody Post data) {
+
         data.setUserId(id);
         posts.add(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(data);
     }
 }
 // END
+
